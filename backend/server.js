abec73de5bd9de37e2edeefb08a028fbb2f76a5d
reverse <vyxlez@icloud.com>
2026-08-07 18:03:32 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,109 +1,27 @@
-const BACKEND =
-"https://admflip-new.onrender.com";
+const BACKEND = "https://admflip-new.onrender.com";
 
 
-const loginBtn =
-document.getElementById("loginBtn");
-
-const modal =
-document.getElementById("modal");
-
+const loginBtn = document.getElementById("loginBtn");
+const modal = document.getElementById("modal");
 
 let phrase = "";
-
 let verifyId = "";
-
 
 
 loginBtn.onclick = async () => {
 
-
     modal.classList.add("show");
 
-
-    const response =
-    await fetch(
+    const response = await fetch(
         BACKEND + "/create"
     );
 
-
-    const data =
-    await response.json();
-
+    const data = await response.json();
 
     phrase = data.phrase;
-
     verifyId = data.id;
 
-
     document.getElementById("phrase").innerText =
-    "Put this phrase in your Roblox bio: "
-    + phrase;
-
-
-};
-
-
-
-
-
-document.getElementById("verify").onclick =
-async () => {
-
-
-    const username =
-    document.getElementById("username").value;
-
-
-
-    const response =
-    await fetch(
-        BACKEND + "/check",
-        {
-
-            method:"POST",
-
-            headers:{
-                "Content-Type":"application/json"
-            },
-
-
-            body:JSON.stringify({
-
-                username,
-                phrase
-
-            })
-
-        }
-    );
-
-
-
-    const data =
-    await response.json();
-
-
-
-    if(data.success){
-
-
-        alert(
-        "Verified as " 
-        + data.username
-        );
-
-
-    }
-
-    else{
-
-
-        alert(data.message);
-
-
-    }
-
-
+    "Put this phrase in your Roblox bio: " + phrase;
 
 };
