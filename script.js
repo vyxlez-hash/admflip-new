a0311877eb8a1aa197721e37568a282b2e921638
reverse <vyxlez@icloud.com>
2026-08-07 17:22:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log("ADMFLIP loaded");
+
+const chatBtn = document.getElementById("chatBtn");
+const chatPanel = document.getElementById("chatPanel");
+
+if(chatBtn && chatPanel){
+
+    chatBtn.addEventListener("click", () => {
+        chatPanel.classList.toggle("active");
+    });
+
+}
+
+
+const online = document.getElementById("online");
+
+if(online){
+
+    setInterval(() => {
+
+        const users = Math.floor(Math.random() * 24) + 32;
+
+        online.innerText = "Online: " + users;
+
+    },30000);
+
+}
+
+
+
+const loginBtn = document.getElementById("loginBtn");
+const modal = document.getElementById("modal");
+
+
+if(loginBtn && modal){
+
+    loginBtn.onclick = () => {
+
+        modal.classList.add("active");
+
+    };
+
+}
