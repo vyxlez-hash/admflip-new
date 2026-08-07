f1d6a8df9ad9c385053da71a8612a4d0a72b8e75
reverse <vyxlez@icloud.com>
2026-08-07 17:31:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,78 @@
-console.log("ADMFLIP loaded");
+const login =
+document.getElementById("loginBtn");
 
-const chatBtn = document.getElementById("chatBtn");
-const chatPanel = document.getElementById("chatPanel");
+const modal =
+document.getElementById("modal");
 
-if(chatBtn && chatPanel){
 
-    chatBtn.addEventListener("click", () => {
-        chatPanel.classList.toggle("active");
-    });
+let phrase="";
+
+
+login.onclick=()=>{
+
+modal.classList.add("show");
+
+
+fetch("https://YOUR-BACKEND.com/create")
+.then(r=>r.json())
+.then(data=>{
+
+phrase=data.phrase;
+
+document.getElementById("phrase").innerText=
+"Put this in your Roblox bio: "+phrase;
+
+});
+
+};
+
+
+
+document.getElementById("verify").onclick=()=>{
+
+
+let username =
+document.getElementById("username").value;
+
+
+fetch(
+"https://YOUR-BACKEND.com/check",
+{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+username,
+phrase
+})
+
+}
+
+)
+
+.then(r=>r.json())
+
+.then(data=>{
+
+
+if(data.success){
+
+alert("Verified!");
+
+}
+
+else{
+
+alert("Verification failed");
 
 }
 
 
-const online = document.getElementById("online");
-
-if(online){
-
-    setInterval(() => {
-
-        const users = Math.floor(Math.random() * 24) + 32;
-
-        online.innerText = "Online: " + users;
-
-    },30000);
-
-}
+});
 
 
-
-const loginBtn = document.getElementById("loginBtn");
-const modal = document.getElementById("modal");
-
-
-if(loginBtn && modal){
-
-    loginBtn.onclick = () => {
-
-        modal.classList.add("active");
-
-    };
-
-}
+};
