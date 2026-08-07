0f8c2194b3d80014d6700b7a4c1e029e346b1352
reverse <vyxlez@icloud.com>
2026-08-07 16:18:25 +0300

    Add Header component with navigation and branding

---
+++ Header.jsx
@@ -0,0 +1,39 @@
+export default function Header(){
+
+return (
+
+<header className="header">
+
+<div className="brand">
+<img src="/logo.png" />
+<span>ADMFLIP</span>
+</div>
+
+
+<nav className="menu">
+
+<a href="#">
+🪙 Coinflip
+</a>
+
+<a href="#">
+🏆 Leaderboard
+</a>
+
+<a href="#">
+💬 Chat
+</a>
+
+</nav>
+
+
+<button className="login">
+Roblox Login
+</button>
+
+
+</header>
+
+)
+
+}
